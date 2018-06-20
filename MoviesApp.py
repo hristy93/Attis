Fixed issues with actors and directors functions, added scaling function for small values (for budget), added extraction and repairs of the data from the imdb movies file

MoviesApp.py
@@ -35,22 +35,31 @@ def get_ratings_by_movie_id(ratings_dataframe, movie_id):
 
 def get_actors_data_by_movie_id(credits_dataframe, movie_id):
     """ Gets the actors data for a movie by id (movie_id) """
+    if not movie_id.isdigit():
+        return {-1:""}
     raw_cast_data = credits_dataframe[credits_dataframe["id"] == int(movie_id)]["cast"]
     cast_data = ast.literal_eval(raw_cast_data.iloc[0])
     actors_data = {actor["id"]: actor["name"] for actor in cast_data}
     #print(cast_data)
     if not cast_data:
-        director_data = {0:""}
+        director_data = {-1:""}
     return actors_data
 
 def get_directors_data_by_movie_id(credits_dataframe, movie_id):
     """ Gets the directors data for a movie by id (movie_id) """
-    raw_crew_data = credits_dataframe[credits_dataframe["id"] == int(movie_id)]["crew"]
-    crew_data = ast.literal_eval(raw_crew_data.iloc[0])
-    director_data = {crew["id"]: crew["name"] for crew in crew_data if crew["job"] == "Director"}
-    #print(crew_data)
-    if not crew_data:
-        director_data = {0:""}
+    if not movie_id.isdigit():
+        return  {-1:""}
+    try:
+        raw_crew_data = credits_dataframe[credits_dataframe["id"] == int(movie_id)]["crew"]
+        if raw_crew_data.empty:
+            return {-1:""}
+        crew_data = ast.literal_eval(raw_crew_data.iloc[0])
+        director_data = {crew["id"]: crew["name"] for crew in crew_data if crew["job"] == "Director"}
+        #print(crew_data)
+        if not crew_data:
+            return {-1:""}
+    except:
+       return {-1:""}
     return director_data
 
 def read_data(file_path):
@@ -72,7 +81,10 @@ def get_all_actors_data(credits_dataframe, movies_metadata_dataframe):
     all_actors_data = list()
     for id in movies_metadata_dataframe_ids:
         result = get_actors_data_by_movie_id(credits_dataframe, id)
-        all_actors_data.append(result.values())
+        if not result:
+            all_actors_data.append(np.nan)
+        else:
+            all_actors_data.append(result.values())
     return all_actors_data
 
 def get_all_directors_data(credits_dataframe, movies_metadata_dataframe):
@@ -81,7 +93,10 @@ def get_all_directors_data(credits_dataframe, movies_metadata_dataframe):
     all_directors_data = list()
     for id in movies_metadata_dataframe_ids:
         result = get_directors_data_by_movie_id(credits_dataframe, id)
-        all_directors_data.append(list(result.keys())[0]) 
+        if not result:
+            all_directors_data.append(np.nan) 
+        else:
+            all_directors_data.append(list(result.keys())[0]) 
     return all_directors_data
 
 def get_one_hot_multilabled_dataframe(data_values, column_name):
@@ -111,6 +126,15 @@ def show_columns_with_nan(dataframe):
     else:
         print("    No")
 
+def scale_small_values(value):
+    """ Scales the value up """
+    if value < 100:
+        return value * 1000000
+    elif value in range(100, 1000):
+        return value * 1000
+    else:
+        return value
+
 def create_testing_dataframe(movies_metadata_dataframe, credits_dataframe):
     """ Creates a dataframe for testing """
     print("\nCreating a testing dataframe for the algorithms ...")
@@ -126,35 +150,49 @@ def create_testing_dataframe(movies_metadata_dataframe, credits_dataframe):
     # Released on friday - 4 == Friday
     df["is_released_on_friday"] = movies_metadata_dataframe["day_of_week"].apply(lambda x: 1 if x == 4 else 0)
     # Released in summer - 6 == June, 7 == July, 8 == August
-    df['is_released_in_summer'] = movies_metadata_dataframe['month'].apply(lambda x: 1 if x in [6, 7, 8] else 0)
+    df["is_released_in_summer"] = movies_metadata_dataframe["month"].apply(lambda x: 1 if x in [6, 7, 8, 11] else 0)
     # Released on hoiday - 4 == April, 5 === May, 6 == June, 11 == November
-    df['is_released_on_holiday'] = movies_metadata_dataframe['month'].apply(lambda x: 1 if x in [4, 5, 6, 11] else 0)
-    df['vote_average'] = movies_metadata_dataframe['vote_average']
+    df["is_released_on_holiday"] = movies_metadata_dataframe["month"].apply(lambda x: 1 if x in [4, 5, 6] else 0)
+    df["vote_average"] = movies_metadata_dataframe["vote_average"]
     #df['vote_average'].replace(np.nan, 0.0, inplace=True)
     #df['vote_average'] = df['vote_average'].fillna(df['vote_average'].mean())
-    df['budget'] = movies_metadata_dataframe['budget']
-    df['vote_count'] = movies_metadata_dataframe['vote_count']
-    df['year'] = movies_metadata_dataframe['year']
+    df["budget"] = movies_metadata_dataframe["budget"].apply(scale_small_values)
+    df["vote_count"] = movies_metadata_dataframe["vote_count"]
+    df["year"] = movies_metadata_dataframe["year"]
+
+    # New features from the imdb movies file
+    #df["nrOfWins"] = movies_metadata_dataframe["nrOfWins"]
+    #df["nrOfNominations"] = movies_metadata_dataframe["nrOfNominations"]
+    #df["nrOfPhotos"] = movies_metadata_dataframe["nrOfPhotos"]
+    #df["nrOfNewsArticles"] = movies_metadata_dataframe["nrOfNewsArticles"]
+    #df["nrOfUserReviews"] = movies_metadata_dataframe["nrOfUserReviews"]
 
     return_data = movies_metadata_dataframe["revenue"].replace(0.0, np.nan) / movies_metadata_dataframe['budget'].replace(0.0, np.nan)
-    df['is_successfull'] = return_data.apply(lambda x: 1 if x >=1 else 0)
-    #print(df[df['is_successfull'].isnull()].shape)
+    df['is_successful'] = return_data.apply(lambda x: 1 if x >=1 else 0)
+    #print(df[df['is_successful'].isnull()].shape)
     
-    # Get the actors count - VERY SLOW !!!
+    ## Get the actors count - VERY SLOW !!!
     #print("  Getting the actors count data ...")
     #raw_cast_data = credits_dataframe["cast"]
     #actors_count_data = [len(ast.literal_eval(item)) for item in raw_cast_data.values]
     #df["actors_count"] = pd.Series(actors_count_data)
 
-    # Get the crew count - VERY SLOW !!!
+    ## Get the crew count - VERY SLOW !!!
     #print("  Getting the crew count data ...")
     #raw_crew_data = credits_dataframe["crew"]
     #crew_count_data = [len(ast.literal_eval(item)) for item in raw_crew_data.values]
     #df["crew_count"] = pd.Series(crew_count_data)
     
-    # Adds the genres as a separate column
+    # Adds some of the genres as a separate column
     genres_one_hot = get_one_hot_multilabled_dataframe(movies_metadata_dataframe, "genres")
-    df = df.join(genres_one_hot)
+    print(genres_one_hot.columns)
+    colums_to_remove = ["Aniplex", "BROSTA TV",
+       "Carousel Productions", "GoHands", "Sentai Filmworks", "The Cartel",
+       "Vision View Entertainment",  "Sentai Filmworks", "Rogue State",
+       "Mardock Scramble Production Committee", "GoHands", "Odyssey Media",
+       "Pulser Productions", "Telescene Film Group Productions"]
+    genres_one_hot_filted = genres_one_hot.drop(columns=colums_to_remove)
+    df = df.join(genres_one_hot_filted)
 
     # Filters the movies based on vote_count colum and a percentile limit - NOT VERY USEFUL
     #percentile = 0.25
@@ -170,13 +208,19 @@ def create_testing_dataframe(movies_metadata_dataframe, credits_dataframe):
 
     columns_to_filter = ["popularity", "runtime", "vote_average",
                          "budget", "vote_count"]
+
+    # New columns from the imdb movies file
+    #new_columns = ["nrOfWins", "nrOfNominations",
+    #                     "nrOfPhotos", "nrOfNewsArticles", "nrOfUserReviews"]
+    #columns_to_filter.extend(new_columns)
+
     print("Filtering the dataframe using only the data that has no NaN values " +\
        "for the columns:\n {}".format(columns_to_filter))
     print("  Shape before filtering: ", df.shape)
     for item in columns_to_filter:
         print(item, " ", df[item].isnull().sum())
         df = df[df[item].notnull()]
-    #df = df[(df["is_successfull"].notnull()) & (df["popularity"].notnull()) & (df["runtime"].notnull())
+    #df = df[(df["is_successful"].notnull()) & (df["popularity"].notnull()) & (df["runtime"].notnull())
     #        & (df["vote_average"].notnull()) & (df["budget"].notnull())]
     print("  Shape after filtering: ", df.shape)
     
@@ -229,8 +273,8 @@ def test_algorithms(movies_metadata_dataframe, credits_dataframe):
 
     # Test 2.2 - Decision tree or gradient boosting classification
     print("Predicting the success of the movies using classification ...")
-    X = df.drop(columns="is_successfull")
-    y = df["is_successfull"]
+    X = df.drop(columns="is_successful")
+    y = df["is_successful"]
 
     test_decision_tree_classification_with_cv(X, y)
     test_gradient_boosting_classification_with_cv(X, y)
@@ -319,6 +363,89 @@ def remove_movies_with_less_votes(dataframe, percentile):
     print("  The movies after filtering: {0}".format(len(dataframe)))
     return dataframe
 
+def extract_and_repair_data_from_new_file(imdb_movies_dataframe):
+    """ Extracts and repairs the data from the imdb movies file """
+    repaired_nrOfWins = []
+    repaired_nrOfNominations = []
+    repaired_nrOfPhotos = []
+    repaired_nrOfNewsArticles = []
+    repaired_nrOfUserReviews = []
+    nrOfWins_items = imdb_movies_dataframe["nrOfWins"]
+    for index, item in enumerate(nrOfWins_items):
+        column = 10;
+        try:
+            if item.isdigit():
+                if int(item) > 1000:
+                    next_item = imdb_movies_dataframe.at[index, "nrOfNominations"]
+                    if not next_item.isdigit():
+                        #repaired_test_imdb.append(np.nan)
+                        #temp_df.shift(periods=-2, axis=1)
+                        column += 2;
+                        #print("preriods: 2")
+                        #next_next_item = imdb_movies_dataframe.at[index, "nrOfPhotos"]
+                        #next_next_next_item = imdb_movies_dataframe.at[index, "nrOfNewsArticles"]
+                        #repaired_nrOfWins.append(next_next_item)
+                        #repaired_nrOfNominations.append(next_next_next_item)
+                    else:
+                        #temp_df.shift(periods=-3, axis=1)
+                        column += 3;
+                        #print("preriods: 3")
+                        #next_next_item = imdb_movies_dataframe.at[index, "nrOfNewsArticles"]
+                        #next_next_next_item = imdb_movies_dataframe.at[index, "nrOfUserReviews"]
+                        #repaired_nrOfWins.append(next_next_item)
+                        #repaired_nrOfNominations.append(next_next_next_item)
+                        
+                #else:
+                    #print("preriods: 0")
+            else:
+                #temp_df.shift(periods=-1, axis=1)
+                column += 1;
+                #print("preriods: 1")
+                #other = imdb_movies_dataframe.at[index, "nrOfNominations"]
+                #next_other = imdb_movies_dataframe.at[index, "nrOfPhotos"]
+                #repaired_nrOfWins.append(other)
+                #repaired_nrOfNominations.append(next_other)
+            new_item = imdb_movies_dataframe.iat[index, column]
+            if not new_item.isdigit():
+                pass
+            repaired_nrOfWins.append(new_item)
+            next_item = imdb_movies_dataframe.iat[index, column + 1]
+            repaired_nrOfNominations.append(next_item)
+            second_next_item = imdb_movies_dataframe.iat[index, column + 2]
+            repaired_nrOfPhotos.append(second_next_item)
+            third_next_item = imdb_movies_dataframe.iat[index, column + 3]
+            repaired_nrOfNewsArticles.append(third_next_item)
+            fourth_next_item = imdb_movies_dataframe.iat[index, column + 4]
+            repaired_nrOfUserReviews.append(fourth_next_item)
+            #print(new_item, next_item, second_next_item, third_next_item, "\n")
+        except:
+            repaired_nrOfWins.append(np.nan)
+            repaired_nrOfNominations.append(np.nan)
+            repaired_nrOfPhotos.append(np.nan)
+            repaired_nrOfNewsArticles.append(np.nan)
+            repaired_nrOfUserReviews.append(np.nan)
+    return (repaired_nrOfWins, repaired_nrOfNominations, repaired_nrOfPhotos, repaired_nrOfNewsArticles, repaired_nrOfUserReviews)
+
+def add_new_columns_to_movies_metadata_dataframe(imdb_movies_dataframe, movies_metadata_dataframe):
+    """ Adds the the imdb movies data to the imdb_movies_dataframe """
+    test_imdb = imdb_movies_dataframe[["tid","nrOfWins", "nrOfNominations", "nrOfPhotos",
+                                      "nrOfNewsArticles", "nrOfUserReviews"]]
+    repaired_nrOfWins, repaired_nrOfNominations, repaired_nrOfPhotos, repaired_nrOfNewsArticles, repaired_nrOfUserReviews =\
+       extract_and_repair_data_from_new_file(imdb_movies_dataframe)
+            
+    test_imdb["nrOfWins"] = edit_data_values(repaired_nrOfWins)
+    test_imdb["nrOfNominations"] = edit_data_values(repaired_nrOfNominations)
+    test_imdb["nrOfPhotos"] = edit_data_values(repaired_nrOfPhotos)
+    test_imdb["nrOfNewsArticles"] = edit_data_values(repaired_nrOfNewsArticles)
+    test_imdb["nrOfnrOfUserReviews"] = edit_data_values(repaired_nrOfUserReviews)
+    print(test_imdb["nrOfWins"].describe())
+    print(test_imdb["nrOfNominations"].describe())
+    print(test_imdb["nrOfPhotos"].describe())
+    print(test_imdb["nrOfNewsArticles"].describe())
+    print(test_imdb["nrOfUserReviews"].describe())
+    movies_metadata_dataframe = movies_metadata_dataframe.copy().join(test_imdb)
+    return movies_metadata_dataframe
+
 def main():
     movies_metadata_test_file_path = "movies_metadata_test.csv"
     movies_metadata_file_path = "files/the-movies-dataset/movies_metadata.csv"
@@ -341,24 +468,12 @@ def main():
     credits_dataframe = read_data(credits_file_path)
 
     # Reads the imdb movies data
-    #print("  Reading the imdb movies' credits ...")
-    #imdb_movies_dataframe = read_data(imdb_movies_file_path)
+    print("  Reading the imdb movies data ...")
+    imdb_movies_dataframe = read_data(imdb_movies_file_path)
 
-    # Gets data from the imdb_movies_movies_dataframe
-    #test_imdb = imdb_movies_dataframe[["tid","nrOfWins", "nrOfNominations"]]
-    #repaired_test_imdb = []
-    #for index, item in enumerate(imdb_movies_dataframe["nrOfWins"]):
-    #    try:
-    #        if item.isdigit():
-    #            repaired_test_imdb.append(item)
-    #        else:
-    #            other = imdb_movies_dataframe.at[index,"nrOfNominations"]
-    #            repaired_test_imdb.append(other)
-    #    except :
-    #        repaired_test_imdb.append(np.nan)
-            
-    #test_imdb["nrOfWins"] = pd.Series(repaired_test_imdb)
-    #movies_metadata_dataframe = movies_metadata_dataframe_old.join(test_imdb.drop(columns="nrOfNominations"))
+    # Adds the the imdb movies data to the imds_movies_dataframe
+    movies_metadata_dataframe = add_new_columns_to_movies_metadata_dataframe(imdb_movies_dataframe,
+                                                                            movies_metadata_dataframe)
 
     #print(len(movies_metadata_dataframe["wins_count"]))
     #print(movies_metadata_dataframe["wins_count"].isnull().sum())